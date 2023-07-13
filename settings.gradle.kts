00ffe8558a5cf46fd2e39a03ca5542e6bf7ea996
Andret2344 <andret2344@gmail.com>
2023-07-13 08:41:47 +0200

    EX: Moved away from groovy in favor of kotlin
    Took 57 seconds

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+/*
+ * Copyright Andret Tools System (c) 2018. Copying and modifying allowed only keeping git link reference.
+ */
+
+pluginManagement {
+	repositories {
+		mavenCentral()
+		gradlePluginPortal()
+	}
+}
+
+rootProject.name = "atsSignTeleport"
